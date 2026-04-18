7b3cf480e0c5cc854cc5cc8a2a37cf2c2ab438cb
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-17 22:22:11 -0700

    initial upload

---
+++ HEVLIB_DRIVERS/REPLACE_TRANSLATIONS.gd
@@ -0,0 +1,144 @@
+extends Node
+
+# This translation file is generated automatically
+# Do not modify anything directly, as this can break things for those working on them
+# Please use Translation Tracker to modify these yourself, and contact the mod author to implement them
+# https://github.com/rwqfsfasxc100/TranslationTracker/releases/latest
+
+const TRANSLATIONS = {
+	"master_locale": "en",
+	"en": {
+		"Be": {
+			"string": "Beryllium",
+			"version_hash": 2579056954
+		},
+		"C": {
+			"string": "Carbon",
+			"version_hash": 2873369242
+		},
+		"Ca": {
+			"string": "Calcium",
+			"version_hash": 324818691
+		},
+		"Ti": {
+			"string": "Titanium",
+			"version_hash": 2845827248
+		},
+		"V": {
+			"string": "Vanadium",
+			"version_hash": 206386394
+		},
+		"Fe": {
+			"string": "Iron",
+			"version_hash": 2089224925
+		},
+		"Ni": {
+			"string": "Nickel",
+			"version_hash": 3312816347
+		},
+		"Cu": {
+			"string": "Copper",
+			"version_hash": 2889915182
+		},
+		"W": {
+			"string": "Tungsten",
+			"version_hash": 936067997
+		},
+		"Re": {
+			"string": "Rhenium",
+			"version_hash": 2783010653
+		},
+		"Pt": {
+			"string": "Platinum",
+			"version_hash": 3028969583
+		},
+		"Au": {
+			"string": "Gold",
+			"version_hash": 2089149675
+		},
+		"Mg2SiO4": {
+			"string": "Forsterite",
+			"version_hash": 2990213548
+		},
+		"Fe2SiO4": {
+			"string": "Fayalite",
+			"version_hash": 1641875604
+		},
+		"MgCaSi2O6": {
+			"string": "Diopside",
+			"version_hash": 3672830646
+		},
+		"NaAlSi2O6": {
+			"string": "Jadeite",
+			"version_hash": 765744347
+		},
+		"CaMn2Si2O6": {
+			"string": "Johannsenite",
+			"version_hash": 3306340107
+		},
+		"NaCr3Si2O6": {
+			"string": "Kosmochlor",
+			"version_hash": 3640712166
+		},
+		"NaMn3Si2O6": {
+			"string": "Namansilite",
+			"version_hash": 1527264506
+		},
+		"NaV3Si2O6": {
+			"string": "Natalyite",
+			"version_hash": 3230076144
+		},
+		"LiAl(SiO3)2": {
+			"string": "Spodumene",
+			"version_hash": 1703492469
+		},
+		"MgSiO3": {
+			"string": "Enstatite",
+			"version_hash": 2248990486
+		},
+		"SiO2": {
+			"string": "Quartz",
+			"version_hash": 3444389836
+		},
+		"SiO2 (Crs)": {
+			"string": "Cristobalite",
+			"version_hash": 3789545706
+		},
+		"SiO2 (Trd)": {
+			"string": "Tridymite",
+			"version_hash": 3841219968
+		},
+		"NaAlSi3O8": {
+			"string": "Albite",
+			"version_hash": 2807576374
+		},
+		"CaAl2Si2O8": {
+			"string": "Anorthite",
+			"version_hash": 2094601171
+		},
+		"KAlSi3O8": {
+			"string": "Orthoclase",
+			"version_hash": 2986417081
+		},
+		"H2O": {
+			"string": "Water",
+			"version_hash": 240943016
+		},
+		"Pd": {
+			"string": "Palladium",
+			"version_hash": 1518614142
+		},
+		"Mg": {
+			"string": "Magnesium",
+			"version_hash": 2166792363
+		},
+		"FULLMINERALS_BRIEF": {
+			"string": "Gives mineral abbreviations their full names",
+			"version_hash": 3968720247
+		},
+		"FULLMINERALS_DESCRIPTION": {
+			"string": "Converts all instances of a mineral's symbol into their full mineral name. Used in places like the mineral market, cargo bay and processed mineral displays, and Geologist identification markers.\n\nAlso supports minerals added by: Hev's More Minerals",
+			"version_hash": 2258301139
+		}
+	}
+}
---
+++ ModMain.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const MOD_PRIORITY = 0
+const MOD_NAME = "Full Mineral Names"
+const MOD_VERSION_MAJOR = 1
+const MOD_VERSION_MINOR = 0
+const MOD_VERSION_BUGFIX = 0
+const MOD_VERSION_METADATA = ""
+func _init(modLoader = ModLoader):
+	l("Initializing")
+func l(msg:String, title:String = MOD_NAME, version:String = str(MOD_VERSION_MAJOR) + "." + str(MOD_VERSION_MINOR) + "." + str(MOD_VERSION_BUGFIX)):
+	if not MOD_VERSION_METADATA == "":
+		version = version + "-" + MOD_VERSION_METADATA
+	Debug.l("[%s V%s]: %s" % [title, version, msg])
