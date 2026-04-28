a15d7c2ac8714a47527996e8340f25889c767fe6
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-27 18:31:44 -0700

    fixed issue that caused any scenes replaced by this mod to cause issues with other mods

--- ModMain.gd
+++ ModMain.gd
@@ -19,7 +19,7 @@ func _init(modLoader = ModLoader):
 	installScriptExtension("MineralSellSlider.gd")
 	replaceScene("hud/components/MineralLabel.tscn")
 	installScriptExtension("SystemMineralList.gd")
-	replaceScene("hud/OMS.tscn")
+#	replaceScene("hud/OMS.tscn")
 
 func l(msg:String, title:String = MOD_NAME, version:String = str(MOD_VERSION_MAJOR) + "." + str(MOD_VERSION_MINOR) + "." + str(MOD_VERSION_BUGFIX)):
 	if not MOD_VERSION_METADATA == "":
@@ -27,12 +27,10 @@ func l(msg:String, title:String = MOD_NAME, version:String = str(MOD_VERSION_MAJ
 	Debug.l("[%s V%s]: %s" % [title, version, msg])
 func installScriptExtension(path:String):
 	var childPath:String = str(modPath + path)
-	var childScript:Script = ResourceLoader.load(childPath,"",true)
+	var childScript:Script = load(childPath)
 	childScript.new()
 	var parentScript:Script = childScript.get_base_script()
 	var parentPath:String = parentScript.resource_path
-	ResourceLoader.load(parentPath,"",true)
-	
 	l("Installing script extension: %s <- %s" % [parentPath, childPath])
 	childScript.take_over_path(parentPath)
 func replaceScene(newPath:String, oldPath:String = ""):
@@ -43,8 +41,7 @@ func replaceScene(newPath:String, oldPath:String = ""):
 
 	newPath = str(modPath + newPath)
 
-	var scene := ResourceLoader.load(newPath,"",true)
+	var scene := load(newPath)
 	scene.take_over_path(oldPath)
 	_savedObjects.append(scene)
-	ResourceLoader.load(oldPath,"",true)
 	l("Finished updating: %s" % oldPath)
