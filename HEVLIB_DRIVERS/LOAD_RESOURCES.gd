38e4b1176d2f6b7268c0f8be2d66e06e7f682036
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-06-09 23:45:33 -0700

    made necessary changes to convert to modlet form

---
+++ HEVLIB_DRIVERS/LOAD_RESOURCES.gd
@@ -0,0 +1,13 @@
+extends Node
+
+const LOAD_RESOURCES = {
+	"ship-ctrl.gd":{"load_type":"script"},
+	"/hud/CargoScanner.gd":{"load_type":"script"},
+	"MineralMark.gd":{"load_type":"script"},
+	"hud/ProcessedCargoManifest.gd":{"load_type":"script"},
+	"CurrentGame.gd":{"load_type":"script"},
+	"MineralLabel.gd":{"load_type":"script"},
+	"MineralSellSlider.gd":{"load_type":"script"},
+	"hud/components/MineralLabel.tscn":{"load_type":"resource","original_path":"res://hud/components/MineralLabel.tscn"},
+	"SystemMineralList.gd":{"load_type":"script"},
+}
